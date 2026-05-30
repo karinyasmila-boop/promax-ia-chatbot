5af8326b8093e241926a55e240150774b00848a7
karinyasmila-boop <karinyasmila@gmail.com>
2026-05-30 16:39:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,54 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title="ADA PROMAX IA", page_icon="🤖")
+st.set_page_config(page_title="ADA PROMAX IA", layout="centered")
 
 st.title("ADA PROMAX IA 🤖 Asistente RRHH - Compensaciones")
 
-# Historial del chat
+# Inicializar historial
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Mostrar mensajes anteriores
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# Mostrar historial
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
 
-# Input del usuario
+# Caja de texto del chat
 prompt = st.chat_input("Escribe tu consulta...")
 
 if prompt:
 
     # Mostrar mensaje usuario
-    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt}
+    )
 
-    st.session_state.messages.append({
-        "role": "user",
-        "content": prompt
-    })
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
+    # URL WEBHOOK MAKE
     webhook_url = "https://hook.us2.make.com/9m7ly3yx7tbtn27ldm63jtg4ljcs52kj"
 
     datos = {
         "employee_id": "PE0000012",
-        "selected_option": "Consulta general",
+        "selected_option": "Consulta General",
         "text": prompt
     }
 
     try:
-
         respuesta = requests.post(webhook_url, json=datos)
 
         if respuesta.status_code == 200:
 
             resultado = respuesta.text
 
+            st.session_state.messages.append(
+                {"role": "assistant", "content": resultado}
+            )
+
             with st.chat_message("assistant"):
                 st.markdown(resultado)
-
-            st.session_state.messages.append({
-                "role": "assistant",
-                "content": resultado
-            })
 
         else:
             st.error("Error conectando con Make")
