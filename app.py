d55c3f5d76b5255cb83371582591750f9d8ec4e4
karinyasmila-boop <karinyasmila@gmail.com>
2026-05-30 16:01:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import requests
 
 st.title("PROMAX IA 🤖 Compensaciones")
 
-employee_id = st.text_input("Ingresa tu código de empleado"),
-[Ejm: PEXXXXXXXX]
+employee_id = st.text_input("Ingresa tu código de empleado")
 
 consulta = st.selectbox(
     "¿Qué deseas consultar?",
